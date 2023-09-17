adbd73244b176e7d095dced5514a40fbf83a8eb0
Zidan Abraham <zidangit07@gmail.com>
2023-09-17 15:49:09 +0800

    feat: add functionality to fetch GitHub user data

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const usernameInput = document.querySelector("#username");
+const fetchBtn = document.querySelector("#fetch-btn");
+const profileContainer = document.querySelector("#profile");
+
+fetchBtn.addEventListener("click", async () => {
+  const username = usernameInput.value;
+  if (!username) {
+    alert("Please enter github username");
+    return;
+  }
+
+  try {
+    const { data } = await axios.get(
+      `https://api.github.com/users/${username}`,
+    );
+    profileContainer.innerHTML = `
+      <h2>${data.login}</h2>
+      <img src="${data.avatar_url}" alt="${data.login}'s avatar">
+      <p>Name: ${data.name || "N/A"}</p>
+      <p>Location: ${data.location || "N/A"}</p>
+      <p>Followers: ${data.followers}</p>
+      <p>Following: ${data.following}</p>
+    `;
+  } catch (error) {
+    profileContainer.innerHTML = "<h1>User not found</h1>";
+  }
+});
