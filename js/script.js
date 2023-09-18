32d650ec12e89e95a13e2656023a474b0d41fcee
Zidan Abraham <zidangit07@gmail.com>
2023-09-18 15:53:05 +0800

    feat(ui): refactor GitHub profile fetching and UI adjustments

--- js/script.js
+++ js/script.js
@@ -1,28 +1,55 @@
 const usernameInput = document.querySelector("#username");
 const fetchBtn = document.querySelector("#fetch-btn");
 const profileContainer = document.querySelector("#profile");
+const profileImg = document.querySelector("#profile-img");
+const profileUsername = document.querySelector("#profile-info h2");
+const profileName = document.querySelector("#profile-info h3");
+const profileLink = document.querySelector("#profile-link");
+const notFound = document.querySelector("#not-found");
 
-fetchBtn.addEventListener("click", async () => {
+fetchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
   const username = usernameInput.value;
+
   if (!username) {
-    alert("Please enter github username");
+    alert("Please enter a GitHub username.");
     return;
   }
 
   try {
-    const { data } = await axios.get(
-      `https://api.github.com/users/${username}`,
-    );
-    console.log(data);
-    profileContainer.innerHTML = `
-      <h2>${data.login}</h2>
-      <img src="${data.avatar_url}" alt="${data.login}'s avatar">
-      <p>Name: ${data.name || "N/A"}</p>
-      <p>Location: ${data.location || "N/A"}</p>
-      <p>Followers: ${data.followers}</p>
-      <p>Following: ${data.following}</p>
-    `;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (response.status === 404) {
+      hideProfile();
+      showNotFound();
+    } else {
+      hideNotFound();
+      const data = await response.json();
+      displayUserProfile(data);
+    }
   } catch (error) {
-    profileContainer.innerHTML = "<h1>User not found</h1>";
+    console.error("Error:", error);
   }
 });
+
+function hideProfile() {
+  profileContainer.classList.remove("show");
+  profileContainer.classList.add("hide");
+}
+
+function showNotFound() {
+  notFound.classList.add("show");
+}
+
+function hideNotFound() {
+  notFound.classList.remove("show");
+  notFound.classList.add("hide");
+}
+
+function displayUserProfile(data) {
+  profileContainer.classList.add("show");
+  profileImg.style.backgroundImage = `url(${data.avatar_url})`;
+  profileUsername.textContent = data.login;
+  profileName.textContent = data.name || "N/A";
+  profileLink.setAttribute("href", data.html_url);
+}
